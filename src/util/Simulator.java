387dc93a6f87e56a27d30d27f495fe360c11b2ff
Fermin De Fiore <dffermin@gmail.com>
2020-07-30 04:09:54 -0300

    -Adding documentation to Simulator

--- src/util/Simulator.java
+++ src/util/Simulator.java
@@ -7,7 +7,18 @@ import interfaces.ISimulator;
 
 import java.util.ArrayList;
 
+/**
+ * Class used to run the ICommands on the IMarsRovers
+ */
 public class Simulator implements ISimulator {
+    /**
+     * Method to execute commands in the rovers
+     *
+     * @param inputIDecoderOutput with the required information to move rovers, it has the plateau, the rovers and the commands
+     * @param logger              logger used to write the output log
+     * @see IDecoderOutput
+     * @see ILoggerOutput
+     */
     public void run(IDecoderOutput inputIDecoderOutput, ILoggerOutput logger) {
         ArrayList<IMarsRover> marsRovers = inputIDecoderOutput.getMarsRovers();
         for (IMarsRover rover : marsRovers) {
