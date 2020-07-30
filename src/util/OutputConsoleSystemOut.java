68739158715da03b080c37428e3dd9de082674c6
Fermin De Fiore <dffermin@gmail.com>
2020-07-30 04:06:17 -0300

    -Adding documentation to OutputConsoleSystemOut

--- src/util/OutputConsoleSystemOut.java
+++ src/util/OutputConsoleSystemOut.java
@@ -5,7 +5,20 @@ import interfaces.IDecoderOutput;
 import interfaces.ILoggerOutput;
 import interfaces.IOutput;
 
+/**
+ * This class represents an IOutput and is used to share information about one rover expedition
+ * into System.out as require the backend application
+ */
 public class OutputConsoleSystemOut implements IOutput {
+
+    /**
+     * Method to expose results by System.out
+     *
+     * @param loggerOutput        ILoggerOutput that contains the log information unused in this class
+     * @param inputIDecoderOutput IDecoderOutput with attributes as the rovers list to get the position needed to share
+     * @see ILoggerOutput
+     * @see IDecoderOutput
+     */
     @Override
     public void exposeResults(ILoggerOutput loggerOutput, IDecoderOutput inputIDecoderOutput) {
         if (inputIDecoderOutput != null) {
@@ -14,6 +27,14 @@ public class OutputConsoleSystemOut implements IOutput {
         }
     }
 
+
+    /**
+     * Private method to convert an OrientedPosition to JSON
+     *
+     * @param orientedPosition Entry oriented position
+     * @return a string with oriented position information as JSON
+     * @see OrientedPosition
+     */
     private String orientedPositionToJson(OrientedPosition orientedPosition) {
         return "{" +
                 "\"xPos\" : " + orientedPosition.getXPos() +
