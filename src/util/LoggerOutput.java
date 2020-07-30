a86c94b6d1eab56b0c3af7778100633f5d6d6f8b
Fermin De Fiore <dffermin@gmail.com>
2020-07-30 03:59:56 -0300

    -Adding documentation to LoggerOutput

--- src/util/LoggerOutput.java
+++ src/util/LoggerOutput.java
@@ -3,8 +3,13 @@ package util;
 
 import interfaces.ILoggerOutput;
 
+/**
+ * This class represents a logger that save every log. It expose method to every possible message to append to the log,
+ * and also have method to append unset messages and clear the log.
+ */
 public class LoggerOutput implements ILoggerOutput {
 
+    //Constants strings used to write the log
     private final String LINE = "\n";
     private final String DOUBLE_LINE = "\n\n";
     private static final String NASA_HISTORY = "------------------------------------NASA EXPEDITION RESULTS------------------------------------ \n\n";
@@ -18,25 +23,46 @@ public class LoggerOutput implements ILoggerOutput {
     private final String ROVERS_LANDED = "*****************MARS ROVERS ARE SUCCESSFULLY LANDED AND STARTING NAVIGATION****************** \n\n";
     private final String ROVERS_OUTPUT = "*******************************MARS ROVERS OUTPUT INFORMATION********************************* \n\n";
     private final String INPUT_ERROR = "*****************THE ROVERS CANT START NAVIGATION BECAUSE OF SOME INPUT ERROR***************** \n\n";
+    //End of constants
 
+    /**
+     * String used to append all the messages
+     */
     private String output = "";
 
+    /**
+     * @return a String with the output information saved
+     */
     @Override
     public String getOutput() {
         return output;
     }
 
+    /**
+     * Method used to append unset messages to the log
+     *
+     * @param msg String with the message
+     */
     @Override
     public void appendSimpleMessage(String msg) {
         output += msg;
         output += LINE;
     }
 
+    /**
+     * Method used to clear the log information
+     */
     @Override
     public void clearResults() {
         output = "";
     }
 
+    /**
+     * Method to append a success decoded message and plateau information
+     *
+     * @param plateauToString Plateau information
+     * @see entities.Plateau
+     */
     @Override
     public void appendPlateauSuccess(String plateauToString) {
         output += PLATEAU_SUCCESS;
@@ -44,12 +70,18 @@ public class LoggerOutput implements ILoggerOutput {
         output += DOUBLE_LINE;
     }
 
+    /**
+     * Method to append an error message
+     */
     @Override
     public void appendErrorMsg() {
         output += INPUT_ERROR;
     }
 
 
+    /**
+     * Method to append the start decoding message
+     */
     @Override
     public void appendStartMsg() {
         output += NASA_HISTORY;
@@ -57,11 +89,18 @@ public class LoggerOutput implements ILoggerOutput {
         output += TRY_DECODE_PLATEAU;
     }
 
+    /**
+     * Method to append message when starting decoding rovers and instructions
+     */
     @Override
     public void appendStartDecodeRAI() {
         output += TRY_DECODE_RAI;
     }
 
+
+    /**
+     * Method to append a decoded rover information
+     */
     @Override
     public void appendDecodedRover(String roverToString) {
         output += DECODED_ROVER;
@@ -69,6 +108,10 @@ public class LoggerOutput implements ILoggerOutput {
         output += LINE;
     }
 
+
+    /**
+     * Method to append the decoded rover's instructions
+     */
     @Override
     public void appendInstructions(String instruction) {
         output += DECODED_INS_SET;
@@ -76,6 +119,10 @@ public class LoggerOutput implements ILoggerOutput {
         output += DOUBLE_LINE;
     }
 
+
+    /**
+     * Method to append an the finish decode message
+     */
     @Override
     public void appendFinishMsg() {
         output += FINISH_DECODE;
@@ -83,6 +130,10 @@ public class LoggerOutput implements ILoggerOutput {
         output += ROVERS_OUTPUT;
     }
 
+
+    /**
+     * Method to append a rover position
+     */
     @Override
     public void appendRoverFinalPosition(String roverToString) {
         output += roverToString;
